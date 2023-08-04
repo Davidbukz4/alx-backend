a3563bcbfd5a0af93147dcf21b4eaceda8f2670b
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-04 02:35:44 +0000

    modified 2-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,7 +22,7 @@ babel = Babel(app)
 @app.route('/')
 def index():
     ''' welcome page '''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 @babel.localeselector
