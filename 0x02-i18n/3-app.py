ca1f3af1e4d7a8af3e5bcd30f5ad6721b714d52f
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-05 02:48:32 +0000

    modified 3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-'''
-Parametrize templates
-'''
-from flask import Flask, render_template
+"""Parametrize templates
+"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    ''' Babel configuration '''
+    """Representation of Babel configuration
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -19,17 +19,21 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """Retrieve request locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/')
-def index():
-    ''' welcome page '''
+def say_hello():
+    """Route handler for 3-index.html
+    """
     return render_template('3-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    ''' Get locale from request '''
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="3030")
